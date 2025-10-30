95283ce0a5552c9bac794bf0a998b4cc9d61e6e8
CatalinaMonserrat <kata1800@gmail.com>
2025-10-29 21:16:43 -0300

    Avance en formulario y views

---
+++ aplicacion/productos/forms.py
@@ -0,0 +1,15 @@
+from django import forms
+from .models import Producto
+
+class ProductoForm(forms.ModelForm):
+    class Meta:
+        model = Producto
+        fields = ['nombre', 'descripcion', 'precio', 'stock', 'categoria', 'etiquetas']
+        widgets = {
+            'nombre': forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Ingrese el nombre del producto'}),
+            'descripcion': forms.Textarea(attrs={'class': 'form-control', 'rows': 3, 'placeholder': 'Ingrese una descripción del producto'}),
+            'precio': forms.NumberInput(attrs={'class': 'form-control', 'min': 0, 'placeholder': 'Ingrese el precio del producto'}),
+            'stock': forms.NumberInput(attrs={'class': 'form-control', 'min': 0, 'placeholder': 'Ingrese el stock del producto'}),
+            'categoria': forms.Select(attrs={'class': 'form-select'}),
+            'etiquetas': forms.SelectMultiple(attrs={'class': 'form-select'}),
+        }
--- aplicacion/productos/views.py
+++ aplicacion/productos/views.py
@@ -1,3 +1,14 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from .models import Producto, Categoria, Etiqueta, DetalleProductos
 
-# Create your views here.
+def index(request):
+    return render(request, 'index.html')
+
+def lista_productos(request):
+    productos = Producto.objects.all(). order_by('-id')
+    return render(request, 'productos/lista.html'), {'productos': productos}
+
+def detalle_producto(request, producto_id):
+    producto = get_object_or_404(Producto, pk=producto_id)
+    return render(request, 'productos/detalle.html', {'producto': producto})
+
